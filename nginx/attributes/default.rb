d19eb7062431563a61d3af5eea037f5837ba6db6
Michael Myers <blackavr@gmail.com>
2012-03-10 14:47:09 -0800

    Added types_hash_max_size and types_hash_bucket_size to attributes and templates

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -62,5 +62,7 @@ default[:nginx][:keepalive_timeout]  = 65
 default[:nginx][:worker_processes]   = cpu[:total]
 default[:nginx][:worker_connections] = 2048
 default[:nginx][:server_names_hash_bucket_size] = 64
+default[:nginx][:types_hash_max_size] = 2048
+default[:nginx][:types_hash_bucket_size] = 64
 
 default[:nginx][:disable_access_log] = false
